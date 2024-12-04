0c52e0a196af72ff2fd8dbf84e5c2c11ffa31241
q_tw_daniel <danielchixxyy@icloud.com>
2024-12-04 20:29:08 +0800

    113/12/04_1.2

---
+++ Vue_2/src/components/MovieCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <MovieItem :description="movie.description" />
+  </div>
+</template>
+<script>
+import MovieItem from "./MovieItem.vue";
+export default {
+  components: {
+    MovieItem,
+  },
+  setup() {
+    const movie = {
+      title: "电影",
+      description: "这是一段电影的描述",
+    };
+
+    return { movie };
+  },
+};
+</script>
+<style scoped>
+.card {
+  padding: 12px;
+  border: 1px solid hsl(240deg, 100%, 80%);
+  border-radius: 4px;
+}
+</style>
--- Vue_2/vite.config.js
+++ Vue_2/vite.config.js
@@ -4,7 +4,4 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
-  }
 });
