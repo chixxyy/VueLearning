437da95d0a3879ca9e47376f2e5aaede2b778878
q_tw_daniel <danielchixxyy@icloud.com>
2025-01-09 20:42:30 +0800

    114/01/09

---
+++ Project/src/App.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+import ShopIcon from './components/Icon/ShopIcon.vue'
+import ProductItem from './components/ProductItem.vue'
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  inStock: boolean;
+  imageUrl: string;
+}
+
+const products = ref<Product[]>([
+  {
+    id: 1,
+    title: "棉T",
+    price: 66,
+    inStock: true,
+    imageUrl: "/images/t-shirt1.jpg",
+  },
+  {
+    id: 2,
+    title: "潮T",
+    price: 72,
+    inStock: true,
+    imageUrl: "/images/t-shirt2.jpg",
+  },
+  {
+    id: 3,
+    title: "牛仔褲",
+    price: 99,
+    inStock: false,
+    imageUrl: "/images/jeans1.jpg",
+  },
+  {
+    id: 4,
+    title: "夾克",
+    price: 128,
+    inStock: true,
+    imageUrl: "/images/jacket1.jpg",
+  },
+])
+</script>
+
+<template>
+  <main>
+    <h1><ShopIcon />加油讚</h1>
+    <div class="productsList">
+      <ProductItem
+        v-for="product in products"
+        :key="product.id"
+        v-bind="product"
+        class="product"
+      >
+      </ProductItem>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: grid;
+  place-items: center;
+  gap: 48px;
+  width: 100vw;
+  padding: 24px;
+}
+
+h1 {
+  display: flex;
+  align-items: center;
+  gap: 32px;
+}
+
+h1 svg {
+  width: 64px;
+  height: 64px;
+}
+.productList {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 48px;
+  width: 60%;
+}
+</style>
---
+++ Project/src/components/ProductItem.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+interface Props {
+    id: number;
+    title: string;
+    price: number;
+    inStock: boolean;
+    imageUrl: string;
+}
+
+defineProps<Props>();
+</script>
+<template>
+    <div class="product">
+        <img :src="imageUrl" :alt="title" />
+        <h2>{{ title }}</h2>
+        <p class="price">售價：{{ price }}元</p>
+        <p>庫存：{{ inStock ? '有貨' : '缺貨' }}</p>
+    </div>
+</template>
+
+<style>
+.product {
+  box-shadow: 0px 0px 24px hsl(0deg, 0%, 0%, 0.1);
+  padding: 18px;
+  border-radius: 12px;
+}
+
+.product img {
+  height: 200px;
+}
+
+.price {
+  color: hsl(0deg, 100%, 66%);
+  font-weight: bold;
+}
+</style>
