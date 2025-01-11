b82b0ca2189427bb833faa2d5b0466be9942810a
q_tw_daniel <danielchixxyy@icloud.com>
2025-01-11 22:35:17 +0800

    114/01/11_1.0

--- Project/src/App.vue
+++ Project/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { ref, type Ref } from 'vue'
-import ShopIcon from './components/Icon/ShopIcon.vue'
-import ProductItem from './components/ProductItem.vue'
+import { ref, type Ref } from 'vue';
+import ShopIcon from './components/Icon/ShopIcon.vue';
+import ProductItem from './components/ProductItem.vue';
+import ActionFilter from './components/ActionFilter.vue';
+import { computed } from 'vue';
 
 interface Product {
   id: number;
@@ -40,20 +42,78 @@ const products = ref<Product[]>([
     inStock: true,
     imageUrl: "/images/jacket1.jpg",
   },
-])
+]);
+
+type SortDirection = 'asc' | 'desc' | "";
+
+interface SortAndFilter {
+  price: SortDirection;
+  name: SortDirection;
+  inStock: boolean | null;
+}
+
+const sortAndFilter = ref<SortAndFilter>({
+  price: "",
+  name: "",
+  inStock: null,
+});
+
+const productResult = computed(() => {
+  return products.value
+    .filter((p) => {
+      return sortAndFilter.value.inStock === null
+        ? true
+        : p.inStock === sortAndFilter.value.inStock;
+    })
+    .sort((a, b) => {
+      if (sortAndFilter.value.price) {
+        return sortAndFilter.value.price === "asc"
+          ? a.price - b.price
+          : b.price - a.price;
+      }
+      if (sortAndFilter.value.name) {
+        return sortAndFilter.value.name === "asc"
+          ? a.title.localeCompare(b.title)
+          : b.title.localeCompare(a.title);
+      }
+      return 0;
+    });
+});
+
+function handSortPrice() {
+  sortAndFilter.value.price =
+    sortAndFilter.value.price === "asc" ? "desc" : "asc";
+  sortAndFilter.value.name = "";
+}
+
+function handSortName() {
+  sortAndFilter.value.name =
+    sortAndFilter.value.name === "asc" ? "desc" : "asc";
+  sortAndFilter.value.price = "";
+}
+
+function handleFilterStock(inStock: boolean | null) {
+  sortAndFilter.value.inStock = inStock;
+}
+
 </script>
 
 <template>
   <main>
     <h1><ShopIcon />加油讚</h1>
+
     <div class="productsList">
+      <ActionFilter 
+        @sort-by-price="handSortPrice"
+        @sort-by-name="handSortName"
+        @filter-stock="handleFilterStock"
+      />
       <ProductItem
-        v-for="product in products"
+        v-for="product in productResult"
         :key="product.id"
         v-bind="product"
         class="product"
-      >
-      </ProductItem>
+      />
     </div>
   </main>
 </template>
@@ -62,9 +122,9 @@ const products = ref<Product[]>([
 main {
   display: grid;
   place-items: center;
-  gap: 48px;
+  gap: 30px;
   width: 100vw;
-  padding: 24px;
+  padding: 20px;
 }
 
 h1 {
@@ -77,10 +137,11 @@ h1 svg {
   width: 64px;
   height: 64px;
 }
+
 .productList {
   display: grid;
   grid-template-columns: 1fr 1fr;
-  gap: 48px;
+  gap: 30px;
   width: 60%;
 }
 </style>
---
+++ Project/src/components/ActionFilter.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+    const emit = defineEmits<{
+        (e: 'sortPrice'): void;
+        (e: 'sortName'): void;
+        (e: 'filterStock', inStock: boolean | null): void;
+    }>()
+
+    function emitFilterStock(e: Event) {
+        const value = (e.target as HTMLSelectElement).value;
+        let inStock: boolean | null = null;
+        if(value) {
+            inStock = value === 'true';
+        }
+        emit('filterStock', inStock);
+    }
+</script>
+<template>
+  <div class="actions">
+    <button @click="emit('sortPrice')">價格排序</button>
+    <button @click="emit('sortName')">名稱排序</button>
+    <select @change="emitFilterStock">
+      <option value="">庫存</option>
+      <option value="true">有貨</option>
+      <option value="false">缺貨</option>
+    </select>
+  </div>
+</template>
+<style scoped>
+.actions {
+  display: flex;
+  gap: 24px;
+  margin-bottom: 24px;
+}
+</style>
